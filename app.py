ec223a18bcba6e8ccc90950c2446dcfea48f330d
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-09-01 14:56:56 -0300

    App basica flask

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py - a minimal flask api using flask_restful
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -d -p 8081:5000 alumnos-ds:latest
