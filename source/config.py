06147758684bdf58ef1cb8e0ed45df33228eb998
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-09-02 18:39:35 -0300

    Primer request y DataSet con pandas

--- source/config.py
+++ source/config.py
@@ -5,3 +5,8 @@ app.config['BACKEND_URL'] = 'http://127.0.0.1:8000/api'
 app.config['API_V2_URL'] = app.config['BACKEND_URL'] + '/v2'
 app.config['TOKEN_URL'] = app.config['BACKEND_URL'] + '/token/'
 app.config['ALUMNOS_URL'] = app.config['API_V2_URL'] + '/alumnos/'
+app.config['MATERIASCURSADAS_URL'] = app.config['API_V2_URL'] + '/materiascursadas/'
+
+
+app.config['USERNAME'] = ''
+app.config['PASSWORD'] = ''
---
+++ source/transformer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class DataTransformer:
+    pass
+
+    def __init__(self, data):
+        """
+            :data is a json
+        """
+        self.data = data
+
+    def transform_to_dataframe(self):
+        return pd.read_json(self.data)
