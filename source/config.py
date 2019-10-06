31512c7cfb613ce0cd949b6d966fbbdf0bf832c8
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-06 12:48:16 -0300

    Cors

--- source/config.py
+++ source/config.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['BACKEND_URL'] = 'http://127.0.0.1:8000/api'
 app.config['API_V2_URL'] = app.config['BACKEND_URL'] + '/v2'
 app.config['TOKEN_URL'] = app.config['BACKEND_URL'] + '/token/'
