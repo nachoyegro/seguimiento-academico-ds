4a611087222c590eb5fefdb4c018f0cda64924ae
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-12 18:59:56 -0300

    Estadisticas basicas sobre materi

--- source/manipulator.py
+++ source/manipulator.py
@@ -2,81 +2,110 @@ import pandas as pd
 
 class DataManipulator:
 
-    def __init__(self, data):
-        """
-            data: Dataframe
-        """
-        self.data = data
-
-    def filtrar_alumnos_de_materia(self, materia):
+    def filtrar_alumnos_de_materia(self, df, materia):
         """
             Quiero obtener los alumnos de una materia
             :return Dataframe
         """
-        df = self.data
-        df = df.loc[df.nota != 'PA'] #No me interesan los Pendientes de Aprobacion
-        df = df.loc[df.materia == materia] #Por ahora lo hago por nombre
+        df = df.loc[df['materia.codigo'] == materia] #Por ahora lo hago por nombre
         return df
 
-    def filtrar_alumnos_de_materia_periodo(self, materia, fecha_inicio, fecha_fin):
+    def filtrar_alumnos_de_materia_periodo(self, df, materia, fecha_inicio, fecha_fin):
         """
             Quiero obtener los alumnos de una materia en un período determinado
             :return Dataframe
         """
-        df = self.filtrar_alumnos_de_materia(materia)
+        df = self.filtrar_alumnos_de_materia(df, materia)
         df = df.loc[(df.fecha >= fecha_inicio) & (df.fecha <= fecha_fin)]
         return df
 
-    def aprobados(self, materia):
+    def aprobados(self, df, materia):
         """
             Obtengo los aprobados en base al resultado, no a la nota
             :return Dataframe
         """
-        df = self.filtrar_alumnos_de_materia(materia)
+        df = self.filtrar_alumnos_de_materia(df, materia)
         df = df.loc[(df.resultado == 'A') | (df.resultado == 'P')] #A: Regular | P: Acredito
         return df
 
-    def desaprobados(self, materia):
+    def desaprobados(self, df, materia):
         """
             Obtengo los desaprobados en base al resultado, no a la nota
             No tiene en cuenta los ausentes
             :return Dataframe
         """
-        df = self.filtrar_alumnos_de_materia(materia)
+        df = self.filtrar_alumnos_de_materia(df, materia)
         df = df.loc[(df.resultado == 'R')] #R: Reprobado
         return df
 
-    def ausentes(self, materia):
+    def ausentes(self, df, materia):
         """
             Obtengo los ausentes en base al resultado
             :return Dataframe
         """
-        df = self.filtrar_alumnos_de_materia(materia)
+        df = self.filtrar_alumnos_de_materia(df, materia)
         df = df.loc[(df.resultado == 'U')] #U: Ausente/Libre
         return df
 
-
-    def alumnos_aprobados_materia_series(self, materia):
+    def alumnos_aprobados_materia_series(self, df, materia):
         """
             Obtengo los alumnos aprobados de una materia
             :return Series
         """
-        aprobados = self.aprobados(materia)
+        aprobados = self.aprobados(df, materia)
         return pd.Series(pd.unique(aprobados['alumno']))
 
-    def alumnos_totales_materia_series(self, materia):
+    def alumnos_totales_materia_series(self, df, materia):
         """
             Obtengo los alumnos totales de una materia
             :return Series
         """
-        alumnos = pd.unique(self.data['alumno'])
+        alumnos = pd.unique(df['alumno'])
         return pd.Series(alumnos)
 
-    def alumnos_falta_aprobar_materia_series(self, materia):
+    def alumnos_falta_aprobar_materia_series(self, df, materia):
         """
             Obtengo los alumnos que aun no aprobaron esta materia
         """
-        aprobados = self.alumnos_aprobados_materia_series(materia)
-        totales = self.alumnos_totales_materia_series(materia)
+        aprobados = self.alumnos_aprobados_materia_series(df, materia)
+        totales = self.alumnos_totales_materia_series(df, materia)
         resultado = totales[~totales.isin(aprobados)]
         return resultado
+
+    #APROBADOS
+    def alumnos_aprobados_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        """
+            Obtengo los alumnos que aprobaron la materia en un determinado período
+        """
+        alumnos_df = self.filtrar_alumnos_de_materia_periodo(df, materia, fecha_inicio, fecha_fin)
+        aprobados_df = self.aprobados(alumnos_df, materia)
+        return aprobados_df
+
+    def cantidad_alumnos_aprobados_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        return len(self.alumnos_aprobados_periodo(df, materia, fecha_inicio, fecha_fin))
+
+    #DESAPROBADOS
+    def alumnos_desaprobados_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        """
+            Obtengo los alumnos que desaprobaron la materia en un determinado período
+        """
+        alumnos_df = self.filtrar_alumnos_de_materia_periodo(df, materia, fecha_inicio, fecha_fin)
+        desaprobados_df = self.desaprobados(alumnos_df, materia)
+        return desaprobados_df
+
+    def cantidad_alumnos_desaprobados_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        return len(self.alumnos_desaprobados_periodo(df, materia, fecha_inicio, fecha_fin))
+
+    #AUSENTES
+    def alumnos_ausentes_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        """
+            Obtengo los alumnos que quedaron ausente en la materia en un determinado período
+        """
+        alumnos_df = self.filtrar_alumnos_de_materia_periodo(df, materia, fecha_inicio, fecha_fin)
+        ausentes_df = self.ausentes(alumnos_df, materia)
+        return ausentes_df
+
+    def cantidad_alumnos_ausentes_periodo(self, df, materia, fecha_inicio, fecha_fin):
+        return len(self.alumnos_ausentes_periodo(df, materia, fecha_inicio, fecha_fin))
+
+    
--- source/tests/tests.py
+++ source/tests/tests.py
@@ -3,28 +3,29 @@ from transformer import DataTransformer
 import pandas as pd
 import numpy as np
 import unittest
+import json
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
         with open('../alumnos_test.json', 'r') as archivo_alumnos:
-            data=archivo_alumnos.read()
+            data=json.loads(archivo_alumnos.read())
             self.dataframe = DataTransformer(data).transform_to_dataframe()
-            self.manipulator = DataManipulator(self.dataframe)
+            self.manipulator = DataManipulator()
 
     def test_alumnos_faltan_aprobar_materia(self):
         """
             El listado de materias tiene 2 alumnos que faltan aprobar la materia COMPUTADORES
         """
-        alumnos = self.manipulator.alumnos_falta_aprobar_materia_series('COMPUTADORES')
+        alumnos = self.manipulator.alumnos_falta_aprobar_materia_series(self.dataframe, '273')
         self.assertEqual(len(alumnos), 2)
 
     def test_alumnos_de_materia(self):
-        alumnos = self.manipulator.filtrar_alumnos_de_materia('QUÍMICA I')
+        alumnos = self.manipulator.filtrar_alumnos_de_materia(self.dataframe,  '628')
         self.assertEqual(len(alumnos), 7)
 
     def test_alumnos_periodo_materia(self):
-        alumnos = self.manipulator.filtrar_alumnos_de_materia_periodo('QUÍMICA I', '2004-01-01', '2007-07-01')
+        alumnos = self.manipulator.filtrar_alumnos_de_materia_periodo(self.dataframe, '628', '2004-01-01', '2007-07-01')
         self.assertEqual(len(alumnos), 3)
 
 if __name__ == '__main__':
