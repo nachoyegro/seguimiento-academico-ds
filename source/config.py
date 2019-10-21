78702d740ca7f6df5558d103ee583f843c598378
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-21 19:14:34 -0300

    Cambio url de backend por la del nombre del contenedor

--- source/config.py
+++ source/config.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['BACKEND_URL'] = 'http://127.0.0.1:8000/api'
+app.config['BACKEND_URL'] = 'http://seguimiento-academico:8000/api'
 app.config['API_V2_URL'] = app.config['BACKEND_URL'] + '/v2'
 app.config['TOKEN_URL'] = app.config['BACKEND_URL'] + '/token/'
 app.config['ALUMNOS_URL'] = app.config['API_V2_URL'] + '/alumnos/'
