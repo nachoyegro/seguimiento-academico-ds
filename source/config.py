fb6f96bff4fa7ec1114caeb50dd1d5fd20e93a8e
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-11-06 10:33:43 -0300

    Guardado de data

--- source/config.py
+++ source/config.py
@@ -3,12 +3,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['BACKEND_URL'] = 'http://seguimiento-academico:8000'
+app.config['BACKEND_URL'] = 'http://127.0.0.1:8000'
 app.config['API_URL'] = app.config['BACKEND_URL'] + '/api'
 app.config['TOKEN_URL'] = app.config['API_URL'] + '/token/'
 app.config['ALUMNOS_URL'] = app.config['API_URL'] + '/alumnos/'
-app.config['MATERIASCURSADAS_URL'] = app.config['API_URL'] + \
+app.config['CARRERAS_URL'] = app.config['API_URL'] + '/carreras/{}'
+app.config['MATERIASCURSADAS_URL'] = app.config['CARRERAS_URL'] + \
     '/materiascursadas/'
+app.config['INSCRIPCIONES_URL'] = app.config['CARRERAS_URL'] + \
+    '/inscripciones/'
+app.config['PLAN_URL'] = app.config['CARRERAS_URL'] + '/planes/{}/'
 app.config['SECRET_KEY'] = 'super-secret'
 
 app.config['USERNAME'] = ''
--- source/provider.py
+++ source/provider.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from config import app
+import os
+
 
 class DataProvider:
 
@@ -11,8 +13,8 @@ class DataProvider:
             :kwargs must have username and password
         """
         token_url = app.config['TOKEN_URL']
-        response = requests.post(token_url, 
-                data=kwargs)
+        response = requests.post(token_url,
+                                 data=kwargs)
         if response.status_code == 200:
             data = json.loads(response.text)
             return data['access']
@@ -21,7 +23,7 @@ class DataProvider:
 
     def get_headers(self, token):
         return {'Authorization': 'Bearer ' + token}
-    
+
     def retrieve_alumnos(self, token):
         """
             :url must be /alumnos, /materias, /carreras...
@@ -35,19 +37,35 @@ class DataProvider:
         else:
             raise Exception
 
-    def retrieve_materiascursadas(self, token):
-        """
-            :url must be /materiascursadas
-            :kwargs
-        """
-        #token = self.retrieve_token(**kwargs)
+    def retrieve_plan(self, token, carrera, plan):
         headers = self.get_headers(token)
-        response = requests.get(app.config['MATERIASCURSADAS_URL'], headers=headers)
+        response = requests.get(app.config['PLAN_URL'].format(
+            carrera, plan), headers=headers)
         if response.status_code == 200:
             return response.text
         else:
             raise Exception
-        
 
+    def get_materiascursadas(self, token, carrera):
+        # TODO: me falta chequear permisos
+        path = 'data/materiascursadas_{}.json'.format(carrera)
+        # Si ya lo traje en otro momento
+        if os.path.isfile(path):
+            with open(path, 'r') as archivo:
+                return json.loads(archivo.read())
+        else:
+            # Si no lo tenia, lo traigo y lo guardo para la proxima
+            data = self.retrieve_materiascursadas(token, carrera)
+            with open(path, 'w+') as archivo:
+                archivo.write(json.dumps(data))
+                archivo.close()
+            return json.loads(data)
 
-
+    def retrieve_materiascursadas(self, token, carrera):
+        headers = self.get_headers(token)
+        response = requests.get(
+            app.config['MATERIASCURSADAS_URL'].format(carrera), headers=headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return []
