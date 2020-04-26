f34dfa3f04afafeb4065f94abcdd878ce51e893d
Juan Ignacio Yegro <nachoyegro@gmail.com>
2020-04-26 13:47:33 -0300

    Termino el server despues de cada test

--- source/tests/test_provider.py
+++ source/tests/test_provider.py
@@ -9,10 +9,12 @@ class ProviderTest(unittest.TestCase):
     def setUp(self):
         self.provider = DataProvider()
         self.mock_app = mock_app
-        self.mock_app.run("localhost", 8008)
+        self.url = "localhost"
+        self.port = 8008
 
     def test_retrieve_alumnos_de_carrera(self):
-        token = self.provider.retrieve_token()
-        data = self.provider.retrieve_alumnos_de_carrera(token, 'W')
-        alumnos = json.loads(data)
-        self.assertEqual(len(alumnos), 10)
+        with self.mock_app.run(self.url, self.port):
+            token = self.provider.retrieve_token()
+            data = self.provider.retrieve_alumnos_de_carrera(token, 'W')
+            alumnos = json.loads(data)
+            self.assertEqual(len(alumnos), 10)
